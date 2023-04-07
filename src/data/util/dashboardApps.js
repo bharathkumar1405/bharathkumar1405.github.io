f15a917164dcb0764d78875c0363fa0a0b95d793
Bharath kumar <bharath.kumar@nrg.com>
2023-04-07 16:26:08 +0530

    added APps

---
+++ src/data/util/dashboardApps.js
@@ -0,0 +1,29 @@
+export const Apps = [
+    {
+        id: "card 1",
+        title: "App1",        
+        image: "https://i.redd.it/b3esnz5ra34y.jpg",
+        path: "/path",
+    },
+    {
+        id: "card 2",
+        title: "App2",        
+        image: "https://source.unsplash.com/user/erondu/600x400",
+        path: "/path2",
+    },
+    {
+        id: "card 3",
+        title: "App3",        
+        image: "https://media.giphy.com/media/10SvWCbt1ytWCc/giphy.gif",
+        path: "/path2",
+    },
+    {
+        id: "card 4",
+        title: "App3",        
+        image: "https://media.giphy.com/media/LwIyvaNcnzsD6/giphy.gif",
+        path: "/path2",
+    },
+
+    
+];
+
